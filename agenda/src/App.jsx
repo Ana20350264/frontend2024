98cbcc0aaf7702ec83e9798b0456d7de021ee144
Ana Lilia Cruz Roque <isc20350264@gmail.com>
2024-02-16 10:24:33 -0600

    Creacion de Agenda

---
+++ agenda/src/App.jsx
@@ -0,0 +1,42 @@
+import ListItem from "./ListItem"
+
+const personas = [
+  {
+    nombre:'Ana Lilia',
+    apellido:'Cruz',
+    email:'ISC20350264@gmail.com',
+    telefono:'287 146 9041',
+  },
+  {
+    nombre:'Guadalupe',
+    apellido:'Roque',
+    email:'guadaluperoque@gmail.com',
+    telefono:'287 883 1244',
+  },
+  {
+    nombre:'Jose Israel',
+    apellido:'Cruz',
+    email:'joetri@gmail.com',
+    telefono:'287 139 5120',
+  },
+]
+function App() {
+
+  return (
+    <div>
+      <h1> Agenda </h1>
+      <hr/>
+      <ul>
+        {personas.map((persona)=>(
+          <ListItem
+          key={persona.email}
+          persona={persona}
+
+          />
+        ))}
+      </ul>
+      </div>
+  )
+}
+
+export default App
